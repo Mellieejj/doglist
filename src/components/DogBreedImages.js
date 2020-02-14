f5af67e36aa6d18d7f4bdafaef5682608c8832d5
Mellieejj <Melanieverheul1992@gmail.com>
2020-02-14 12:10:41 +0100

    add dog images

---
+++ src/components/DogBreedImages.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function DogBreedImages(props) {
+  const { images, breed } = props;
+  return (
+    <div className="dog-breed-images">
+      <h1>Dogs {breed} Breed Images</h1>
+      This page will show images of the {breed} breed.
+      <div>
+        {" "}
+        {images && images.map(url => <img src={url} alt="Dog" />)}
+        {!images && "Loading..."}
+      </div>
+      <Link to="/">Go back to the index</Link>
+    </div>
+  );
+}
--- src/components/DogList.js
+++ src/components/DogList.js
@@ -1,37 +1,22 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class DogsList extends Component {
-  state = {
-    dogBreeds: null
-  };
-
-  componentDidMount() {
-    fetch("https://dog.ceo/api/breeds/list/all")
-      .then(res => res.json())
-      .then(data => {
-        const breeds = Object.keys(data.message);
-        this.updateBreeds(breeds);
-      })
-      .catch(console.error);
-  }
-
-  updateBreeds(breeds) {
-    this.setState({
-      dogBreeds: breeds
-    });
+  renderDogBreed(breed) {
+    return (
+      <li key={breed}>
+        <Link to={`/dog-breeds/${breed}`}>{breed}</Link>
+      </li>
+    );
   }
 
   render() {
+    const { dogBreeds } = this.props;
     return (
       <div className="dogs-list">
         <h1>Dogs List</h1>
-        {this.state.dogBreeds === null && "Loading...."}
-        {
-          <ul>
-            {this.state.dogBreeds !== null &&
-              this.state.dogBreeds.map(breed => <li key={breed}>{breed}</li>)}
-          </ul>
-        }
+        {!dogBreeds && "Loading...."}
+        {dogBreeds && <ul>{dogBreeds.map(this.renderDogBreed)} </ul>}
       </div>
     );
   }
